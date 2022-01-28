ab7eaf1ce3a95141babcf43b25246099f602c857
Vacarsu <kasvaca@gmail.com>
2022-01-27 18:12:54 -0800

    run put_child in correct spot

--- lib/text_input/text_input.ex
+++ lib/text_input/text_input.ex
@@ -66,12 +66,12 @@ defmodule FloUI.TextInput do
 
   @impl true
   def process_update(data, opts, scene) do
+    put_child(scene, :text_input, data)
     {:noreply, assign(scene, data: data, value: data, opts: opts)}
   end
 
   @impl true
   def process_event({:value_changed, :text_input, value}, _, %{assigns: %{id: id}} = scene) do
-    put_child(scene, :text_input, value)
     {:cont, {:value_changed, id, value}, assign(scene, value: value)}
   end
 
